efaef96d218d6fc73f0a811f99bf02aff07d62ae
Shanto <shantofficial14500@gmail.com>
2026-04-12 20:46:50 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:my_task/myApp.dart';
+
+void main() => runApp(MyApp());
---
+++ lib/myApp.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
